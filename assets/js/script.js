f762a233ea52cbd966d960bd784da5bace95db92
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2023-09-18 12:36:00 +0200

    add function to display user and computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+let playerScore = 0;
+let computerScore = 0;
+
+/**
+ * The different choices for the game 
+ */
+function game(playerChoice) {
+    const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+    const computerChoice = choices[Math.floor(Math.random() * 5)];
+    const result = checkWinner(playerChoice, computerChoice);
+
+    updateScores(result);
+}
+
+/**
+ * Displays the choice made by the user and computer and shows who won 
+ */
+function displayResult(playerChoice, computerChoice, result) {
+    const message = document.getElementById('message');
+    message.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}. You ${result}!`;
+}
+
+
+
 /**
  * Start screen
  */
