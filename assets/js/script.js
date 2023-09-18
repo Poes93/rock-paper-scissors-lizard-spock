f4f7c914239d44b73f3b28e4f77de2d4b096b9b2
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2023-09-18 13:36:55 +0200

    add score counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -9,8 +10,10 @@ function game(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 5)];
     const result = checkWinner(playerChoice, computerChoice);
 
+    updateScores(result);
+    displayScores();
+    displayResult(playerChoice, computerChoice, result);
 }
-
 /**
  * Displays the choice made by the user and computer and shows who won 
  */
@@ -18,21 +21,36 @@ function displayResult(playerChoice, computerChoice, result) {
     const message = document.getElementById('message');
     message.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}. You ${result}!`;
 }
-
-/**
- * Displays the choice made by the user and computer and shows who won 
- */
-function displayResult(playerChoice, computerChoice, result) {
-    const message = document.getElementById('message');
-    message.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}. You ${result}!`;
-}
-
 /**
  * Finds the score of wins for both the player and computer 
  */
 function displayScores() {
     const scoreMessage = document.getElementById('score');
     scoreMessage.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+}
+/**
+ * Checks the rules of the game to see if the player won or lost 
+ */
+function checkWinner(player, computer) {
+    if (player === computer) return "draw";
+
+    if ((player === "rock" && (computer === "scissors" || computer === "lizard")) ||
+        (player === "paper" && (computer === "rock" || computer === "spock")) ||
+        (player === "scissors" && (computer === "paper" || computer === "lizard")) ||
+        (player === "lizard" && (computer === "spock" || computer === "paper")) ||
+        (player === "spock" && (computer === "scissors" || computer === "rock"))) {
+        return "win";
+    } else {
+        return "lose";
+    }
+}
+// Score keeper
+function updateScores(result) {
+    if (result === "win") {
+        playerScore++;
+    } else if (result === "lose") {
+        computerScore++;
+    }
 }
 
 /**
@@ -42,3 +60,4 @@ function startGame() {
     const startScreen = document.getElementById('start-screen');
     startScreen.style.display = 'none';
 }
+
